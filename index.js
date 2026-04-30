d747897362ef9745999039f2a69382186254fb23
jackooneplus-rgb <jackooneplus@gmail.com>
2026-04-30 18:38:56 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// NOTE MODAL LOGIC
+window.openNote = function () {
+  document.getElementById("noteModal").classList.remove("hidden");
+};
+
+window.closeNote = function () {
+  document.getElementById("noteModal").classList.add("hidden");
+};
