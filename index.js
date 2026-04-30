eac17ff44af6c97e88380271200a59983e84ef15
jackooneplus-rgb <jackooneplus@gmail.com>
2026-04-30 19:23:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
-const homePage = document.getElementById("homePage");
-const songPage = document.getElementById("songPage");
-const noteModal = document.getElementById("noteModal");
-const music = document.getElementById("bgMusic");
+document.addEventListener("DOMContentLoaded", () => {
 
-function goToSong() {
-  homePage.classList.add("hidden");
-  songPage.classList.remove("hidden");
-}
+  const homePage = document.getElementById("homePage");
+  const songPage = document.getElementById("songPage");
+  const noteModal = document.getElementById("noteModal");
+  const music = document.getElementById("bgMusic");
 
-function goHome() {
-  songPage.classList.add("hidden");
-  homePage.classList.remove("hidden");
-}
-
-function openNote() {
-  noteModal.classList.remove("hidden");
-}
-
-function closeNote() {
+  // 🔥 FORCE modal hidden on load (this fixes your bug)
   noteModal.classList.add("hidden");
-}
 
-/* 🎵 SOFT MUSIC WITH FADE */
-function toggleMusic() {
-  if (music.paused) {
-    music.volume = 0;
-    music.play();
+  window.goToSong = function () {
+    homePage.classList.add("hidden");
+    songPage.classList.remove("hidden");
+  };
 
-    let vol = 0;
-    const fade = setInterval(() => {
-      if (vol < 0.2) {
-        vol += 0.02;
-        music.volume = vol;
-      } else {
-        clearInterval(fade);
-      }
-    }, 100);
+  window.goHome = function () {
+    songPage.classList.add("hidden");
+    homePage.classList.remove("hidden");
+  };
 
-  } else {
-    music.pause();
-  }
-}
+  window.openNote = function () {
+    noteModal.classList.remove("hidden");
+  };
+
+  window.closeNote = function () {
+    noteModal.classList.add("hidden");
+  };
+
+  window.toggleMusic = function () {
+    if (music.paused) {
+      music.volume = 0;
+      music.play();
+
+      let vol = 0;
+      const fade = setInterval(() => {
+        if (vol < 0.2) {
+          vol += 0.02;
+          music.volume = vol;
+        } else {
+          clearInterval(fade);
+        }
+      }, 100);
+    } else {
+      music.pause();
+    }
+  };
+
+});
