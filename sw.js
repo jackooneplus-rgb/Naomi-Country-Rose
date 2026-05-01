20ee7d126bedf680f010ad5442dfc2614034fc65
jackooneplus-rgb <jackooneplus@gmail.com>
2026-05-01 11:49:41 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "naomi-country-rose-v1600";
+
+self.addEventListener("install", function () {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request);
+    })
+  );
+});
